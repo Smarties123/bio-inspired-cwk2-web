52f5fac12dd4f5a38c742dda63fa3026b21a6bf1
Hemant <hemmant.smart@gmail.com>
2025-05-03 16:20:03 +0100

    api env change for prod

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-const api = axios.create({ baseURL: "http://localhost:8000" });
+// const api = axios.create({ baseURL: "http://localhost:8000" });
+const api = axios.create({
+      baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+});
 export const generateNoise = (payload) => api.post("/generate_noise", payload);
 export const runCA  = (payload) => api.post("/run_ca", payload);
 export const runHop = (payload) => api.post("/run_hopfield", payload);
